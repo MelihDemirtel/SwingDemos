3414baf26387ea0ffefbc64acf5e418458da543b
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-26 09:56:34 +0300

    demo project created

---
+++ swingDemos/demo/src/main/java/demo/demo/mavenproject1/Demo.java
@@ -0,0 +1,12 @@
+package demo.demo.mavenproject1;
+
+/**
+ *
+ * @author MelihDemirtel
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
